d9451e15efcb86fe0e82cb2b6a5b4d94155cfb18
orrlevinger <mr.levinger@gmail.com>
2023-05-17 12:02:45 +0300

    test

--- aws/s3-bucket/s3/s3.tf
+++ aws/s3-bucket/s3/s3.tf
@@ -16,11 +16,6 @@ resource "aws_s3_bucket" "website_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket" "website_bucket2" {
-  bucket = "env0-s3-1${random_string.random.result}"
-  force_destroy = true
-}
-
 resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
   bucket = aws_s3_bucket.website_bucket.id
   rule {
@@ -31,7 +26,7 @@ resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
 resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
