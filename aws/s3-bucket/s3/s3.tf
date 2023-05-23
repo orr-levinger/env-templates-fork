cc26637b6de38869f2b06f2f3c15ece27d781db6
orrlevinger <mr.levinger@gmail.com>
2023-05-23 15:30:50 +0300

    test

--- aws/s3-bucket/s3/s3.tf
+++ aws/s3-bucket/s3/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
 resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
