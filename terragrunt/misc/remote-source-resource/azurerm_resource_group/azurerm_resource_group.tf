af24a3e90c24df86a235a923f7b68552e395e024
orrlevinger <mr.levinger@gmail.com>
2023-07-25 14:15:17 +0300

    pr-comment-test

--- terragrunt/misc/remote-source-resource/azurerm_resource_group/azurerm_resource_group.tf
+++ terragrunt/misc/remote-source-resource/azurerm_resource_group/azurerm_resource_group.tf
@@ -8,9 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  }
+  features {}
+  subscription_id = "your_subscription_id"
+  client_id       = "3f943d15-e69e-4313-a3bf-0a7d55caa174"
+  client_secret   = ".fF8Q~FxUp8.fTRg.bfEHmDnFjB2-AXsTBGrAbx~"
+  tenant_id       = "f3450d00-1632-47b8-ab1b-c7c1617ef6cd"
 }
 
 resource "azurerm_resource_group" "example" {
