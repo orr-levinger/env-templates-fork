66945b2e1b79a1f39c855ac7ab84e9ae038c304f
orrlevinger <mr.levinger@gmail.com>
2023-07-27 12:58:59 +0300

    pr-comment-test

--- terragrunt/misc/remote-source-resource/azurerm_resource_group/azurerm_resource_group.tf
+++ terragrunt/misc/remote-source-resource/azurerm_resource_group/azurerm_resource_group.tf
@@ -9,10 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "f8c64666-d2f2-4bb8-ba6c-b83178bd8b72"
-  client_id       = "3f943d15-e69e-4313-a3bf-0a7d55caa174"
-  client_secret   = ".fF8Q~FxUp8.fTRg.bfEHmDnFjB2-AXsTBGrAbx~"
-  tenant_id       = "f3450d00-1632-47b8-ab1b-c7c1617ef6cd"
 }
 
 resource "azurerm_resource_group" "example" {
